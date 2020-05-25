a57c311dbd917c0d84be9d17b89adf7174b1da1c
Alan <alanhaugen@gmail.com>
2020-05-25 13:40:32 +0200

    Fleshing out vector class. Addded favicon

--- vec3.h
+++ vec3.h
@@ -22,6 +22,38 @@ public:
     inline float operator[](int i) const { return e[i]; }
     inline float &operator[](int i) { return e[i]; }
 
+    inline vec3 &operator+=(const vec3 &v2);
+    inline vec3 &operator-=(const vec3 &v2);
+    inline vec3 &operator*=(const vec3 &v2);
+    inline vec3 &operator/=(const vec3 &v2);
+    inline vec3 &operator*=(const float t);
+    inline vec3 &operator/=(const float t);
+
+    inline std::istream &operator>>(std::istream &is)
+    {
+        is >> e[0] >> e[1] >> e[2];
+        return is;
+    }
+
+    inline std::ostream &operator<<(std::ostream &os)
+    {
+        os << e[0] << " " << e[1] << " " << e[2];
+        return os;
+    }
+
+    inline void makeUnitVector()
+    {
+        float k = 1.0 / sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
+        e[0] *= k;
+        e[1] *= k;
+        e[2] *= k;
+    }
+
+    inline vec3 operator+(const vec3 &v1, const vec3 &v2)
+    {
+        return vec3();
+    }
+
     inline float length() const
     {
         return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
@@ -30,7 +62,6 @@ public:
     {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
-    inline void makeUnitVector();
 
     float e[3];
 };

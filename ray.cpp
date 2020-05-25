d6be2e38f44bc302e7232e8a4a2f4e904b45a0a2
Alan <alanhaugen@gmail.com>
2020-05-25 15:54:29 +0200

    Added sphere to scene

--- ray.cpp
+++ ray.cpp
@@ -9,15 +9,31 @@
 #include "stb_image.h"
 #include "stb_image_write.h"
 
+bool hitSphere(const vec3 &center, float radius, const ray &r)
+{
+    vec3 oc = r.origin() - center;
+
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius*radius;
+
+    float discriminant = b*b - 4*a*c;
+
+    return (discriminant > 0);
+}
 
 vec3 color(const ray &r)
 {
+    if (hitSphere(vec3(0, 0, -1), 0.5, r))
+    {
+        return vec3(1, 0, 0);
+    }
+
 	vec3 unitDirection = unitVector(r.direction());
 	float t = 0.5 * (unitDirection.y() + 1.0);
 
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
-
 
 int main()
 {

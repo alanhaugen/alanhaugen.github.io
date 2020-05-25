04aec179d9d5171d6e51d9ab1d9a5a6c6eeecaa1
Alan <alanhaugen@gmail.com>
2020-05-25 12:15:17 +0200

    Completed chapter 3, however vec3 does not work
    Support for multiplying a vector with a constant has not been
    implemented

--- ray.cpp
+++ ray.cpp
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//#include "ray.h"
+#include "ray.h"
 #include "vec3.h"
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image.h"
 #include "stb_image_write.h"
 
-/*
+
 vec3 color(const ray &r)
 {
 	vec3 unitDirection = unitVector(r.direction());
@@ -17,34 +17,42 @@ vec3 color(const ray &r)
 
 	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
-*/
+
 
 int main()
 {
 	int nx = 200;
 	int ny = 100;
-    int channels = 3;
+	int channels = 3;
 
-    uint8_t data[3 * 200 * 100];
+	uint8_t data[3 * 200 * 100];
 
-    int index = 0;
+	vec3 lowerLeftCorner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
+
+	int index = 0;
 
 	for (int j = ny - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+			float u = float(i) / float(nx);
+			float v = float(j) / float(ny);
 
-            data[index++] = int(255.99 * col[0]);
-            data[index++] = int(255.99 * col[1]);
-            data[index++] = int(255.99 * col[2]);
+			ray r(origin, lowerLeftCorner + u*horizontal + v*vertical);
+
+			vec3 col = color(r); 
+
+			data[index++] = int(255.99 * col[0]);
+			data[index++] = int(255.99 * col[1]);
+			data[index++] = int(255.99 * col[2]);
 		}
 	}
 
-    int success = stbi_write_png("img.png", nx, ny, channels, data, nx * channels);
+	int success = stbi_write_png("img.png", nx, ny, channels, data, nx * channels);
 
-    return success;
-
-	//vec3 lowerLeftCorner(-2.0, -1.0, -1.0);
+	return success;
 }
 

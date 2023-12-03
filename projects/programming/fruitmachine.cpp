79c290155262f1373b90bd0ccf8b88f1224993ab
Alan <alanhaugen@gmail.com>
2023-12-03 13:18:53 +0100

    Finished all examples in programming project

--- projects/programming/fruitmachine.cpp
+++ projects/programming/fruitmachine.cpp
@@ -4,10 +4,17 @@ using namespace std;
 
 int main()
 {
+	// This will output ♣ ♦ ♥ ♠ if run in a Windows CLI (on windows)
 	cout << char(3) << endl;
 	cout << char(4) << endl;
 	cout << char(5) << endl;
 	cout << char(6) << endl;
+
+	// This will print out ASCII in the official standard range (7 bits)
+	for (int i = 0; i < 128; i++)
+	{
+		cout << char(i) << endl;
+	}
 
 	return 0;
 }
